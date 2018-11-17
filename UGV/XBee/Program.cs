08cbaeea906884922f0ca81917d104a91ab6252e
Paul Matthew Barrameda <luisanduis729@gmail.com>
2018-11-16 20:42:18 -0800

    Added UGV Communication - Needs Work
    Does not work yet. Needs work on receiving information.

---
+++ UGV/XBee/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace UGV.XBee
+{
+    public struct Config
+    {
+        public const string Port = "COM5";
+        public const int BaudRate = 57600;
+        public const string DestinationMAC = "0013A20040917A31";
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            XBeeDevice xbee = new XBeeDevice(Config.Port, Config.BaudRate, Config.DestinationMAC);
+            XBeeDevice xbee_r = new XBeeDevice("COM6", 57600);
+
+            bool success1 = xbee.Start();
+            bool success2 = xbee_r.Start();
+            if (!success1 || !success2)
+            {
+                Console.WriteLine("Failed to initialize XBees");
+                Console.ReadLine();
+                return;
+            }
+
+            string input;
+            XBeePacket packet;
+            Random rnd = new Random();
+            while (true)
+            {
+                Console.Write("Type anything to send a packet (-1 to exit): ");
+                input = Console.ReadLine();
+                if (input.Equals("-1")) break;
+
+                xbee.Send(rnd.Next(1, 50));
+                /*
+                packet = new XBeePacket(rnd.Next(1,50), rnd.Next(1, 50));
+                xbee.Send(packet);
+                */
+            }
+
+            xbee.Stop();
+            xbee_r.Start();
+        }
+    }
+}
---
+++ UGV/XBee/XBeePacket.cs
@@ -0,0 +1,35 @@
+﻿namespace UGV.XBee
+{
+    public struct XBeePacket
+    {
+        public int latitude, longitude;
+        public XBeePacket(int lat, int lng)
+        {
+            latitude = lat;
+            longitude = lng;
+        }
+
+        public override bool Equals(object obj)
+        {
+            XBeePacket xbee = (XBeePacket)obj;
+
+            return latitude == xbee.latitude &&
+                longitude == xbee.longitude;
+        }
+
+        public override int GetHashCode()
+        {
+            return (latitude + 90) * 180 + longitude;
+        }
+
+        public static bool operator ==(XBeePacket left, XBeePacket right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(XBeePacket left, XBeePacket right)
+        {
+            return !(left == right);
+        }
+    }
+}
