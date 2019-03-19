6f45b5b9b3365c3d58e8ffc2a21838bd6e84f3a6
Enrique Hernandez <enrq519@gmail.com>
2019-03-18 20:55:12 -0700

    class for message types added

---
+++ UGVComms/UGVComms/msgClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UGVComms
+{
+    public class connectMsg
+    {
+        public const string type = "connect";
+        public int time { get; set; }
+        public string jobsAvailable { get; set; }
+    }
+
+    public class updateMsg
+    {
+        public const string type = "update";
+        public float lat { get; set; }
+        public float lon { get; set; }
+        public float heading { get; set; }
+        public string status { get; set; }
+    }
+
+    public class ackMsg
+    {
+        public const string type = "ack";
+    }
+
+    public class completeMsg
+    {
+        public const string type = "complete";
+    }
+}
