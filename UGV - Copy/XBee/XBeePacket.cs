ef96e27df4dc1e7da4a98b96dfe8a3acb47d7fc0
Enrique Hernandez <enrq519@gmail.com>
2019-03-23 03:31:01 -0700

    added method to classes to get Epoch time + added comments

---
+++ UGV - Copy/XBee/XBeePacket.cs
@@ -0,0 +1,42 @@
+﻿namespace UGV.XBee
+{
+    // feel free to modify this as we set up the structure of the packets we
+    // are sending as JSON through the xbee
+    public struct XBeePacket
+    {
+        public int latitude, longitude;
+        public XBeePacket(int lat, int lng)
+        {
+            latitude = lat;
+            longitude = lng;
+        }
+
+        public override bool Equals(object obj)
+        {
+            XBeePacket xbee = (XBeePacket)obj;
+
+            return latitude == xbee.latitude &&
+                longitude == xbee.longitude;
+        }
+
+        public override int GetHashCode()
+        {
+            return (latitude + 90) * 180 + longitude;
+        }
+
+        public static bool operator ==(XBeePacket left, XBeePacket right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(XBeePacket left, XBeePacket right)
+        {
+            return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return "latitude: " + latitude + "\nlongitude: " + longitude;
+        }
+    }
+}
--- UGVComms/UGVComms/msgClass.cs
+++ UGVComms/UGVComms/msgClass.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace UGVComms
 {
+    /* Classes are being used instead of structs because structs are considered value types
+     * which means they are copied when they are passed around. 
+     * 
+     * If you change a copy, changes are only made to that specific copy, and not to the 
+     * original nor to any other copies around in use that you may have wanted to change as well.
+     * 
+     * Manipulating a struct's properties as you would a class would require you pass the struct
+     * by reference, which such an implementation is significantly more prone to hard-to-catch 
+     * mistakes, as well as reduced code maintainability. 
+     * 
+     * Using structs for this implementation would prove to be less efficient and not worth
+     * the effort as performance increases are negligible when comparing classes and structs.
+     */
+
     public class MsgClass
     {
         public string type;
@@ -15,7 +29,7 @@ namespace UGVComms
     public class ConnectMsg : MsgClass
     {
         public new const string type = "connect";
-        public int Time { get; set; }
+        public int Time = (int)DateTimeOffset.Now.ToUnixTimeSeconds();   // method for getting Epoch time, og type is long, casted to int
         public string jobsAvailable = "searchAndRescue";
     }
 
@@ -39,24 +53,24 @@ namespace UGVComms
     }
     
     //////////////messages to be received/////////////
-    public class ConnAckMsg : MsgClass
+    public class ConnAckMsg : MsgClass      
  	{
         public new string type;
-        public int Time;
+        public int Time = (int)DateTimeOffset.Now.ToUnixTimeSeconds();
  	}
  	
- 	public class RecAckMsg : MsgClass 
+ 	public class RecAckMsg : MsgClass       
  	{
         public new string type;
  	}
  	
- 	public class StartMsg : MsgClass 
+ 	public class StartMsg : MsgClass        
  	{
         public new string type;
         public string jobType;
  	}
  	
- 	public class AddMissionMsg : MsgClass 
+ 	public class AddMissionMsg : MsgClass   
  	{
         public new string type;
         public int lat;
