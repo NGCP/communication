41fb874d4a1b6a525028ab84f54a6074a94053e3
Paul Matthew Barrameda <luisanduis729@gmail.com>
2019-04-15 17:08:27 -0700

    Use msgpack to compress JSON messages

--- UAV/json.cpp
+++ UAV/json.cpp
@@ -1,14 +1,53 @@
-#include "ThorSerialize/Traits.h"
-#include "ThorSerialize/JsonThor.h"
+#include <msgpack.hpp>
+#include <iostream>
 
-struct XbeePacket {
-	int latitude;
-	int longitude;
+using namespace std;
+
+class ConnectMessage {
+private:
+	string type;
+	unsigned long int id;
+	unsigned long int sid;
+	unsigned long int tid;
+	unsigned long long int time;
+	
+public:
+	ConnectMessage(float i, unsigned long int s, unsigned long int t, unsigned long long int ti) {
+		type = "connect";
+		id = i;
+		sid = s;
+		tid = t;
+		time = ti;
+	}
+	
+	ConnectMessage() {
+		type = "connect";
+		id = 0;
+		sid = 0;
+		tid = 0;
+		time = 0;
+	}
+	
+	string toString() {
+		return "type: " + type + " id: " + to_string(id) + " sid: " + to_string(sid) + " tid: " + to_string(tid) + " time: " + to_string(time);
+	}
+
+	MSGPACK_DEFINE(type, id, sid, tid, time);
 };
-ThorsAnvil_MakeTrait(XbeePacket, latitude, longitude);
 
 int main() {
-	XbeePacket packet{1,2};
-	std::stringstream s;
-	s << ThorsAnvil::Serialize::jsonExport(packet, ThorsAnvil::Serialize::PrinterInterface::OutputType::Stream);
+	ConnectMessage packet(0, 100, 0, (std::chrono::system_clock::now().time_since_epoch()).count());
+	
+	cout << packet.toString() << endl;
+	
+	msgpack::sbuffer sbuf;
+	msgpack::pack(sbuf, packet);
+	
+	msgpack::object_handle oh = msgpack::unpack(sbuf.data(), sbuf.size());
+	msgpack::object obj = oh.get();
+	
+	ConnectMessage packet2;
+	obj.convert(packet2);
+	
+	cout << packet2.toString() << endl;
 }
