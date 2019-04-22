995501f28e8e96f1800d60b7efacbd776bedee6d
Enrique Hernandez <enrq519@gmail.com>
2019-04-22 01:11:36 -0700

    updated comments

--- UGVComms/UGVComms/MsgClass.cs
+++ UGVComms/UGVComms/MsgClass.cs
@@ -2,19 +2,8 @@
 
 namespace UGVComms
 {
-    /* Classes are being used instead of structs because structs are considered value types
-     * which means they are copied when they are passed around. 
-     * 
-     * If you change a copy, changes are only made to that specific copy, and not to the 
-     * original nor to any other copies around in use that you may have wanted to change as well.
-     * 
-     * Manipulating a struct's properties as you would a class would require you pass the struct
-     * by reference, which such an implementation is significantly more prone to hard-to-catch 
-     * mistakes, as well as reduced code maintainability. 
-     * 
-     * Using structs for this implementation would prove to be less efficient and not worth
-     * the effort as performance increases are negligible when comparing classes and structs.
-     */
+     // *** Name all float/long types to double
+
     [MessagePackObject]
     public class MsgClass
     {
@@ -142,8 +131,6 @@ namespace UGVComms
             Type = "stop";
         }
     }
-
-    //////////////other messages//////////////
 
     public class AckMsg : MsgClass
     {
